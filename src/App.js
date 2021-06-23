138aa9e084f5ac4d0aabe6b09af4b012d60e4287
Teeratham Vitchutripop <teerathamvitchutripop@Teerathams-MBP.fios-router.home>
2021-06-23 14:24:02 -0400

    Initialized React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="center" style={{justifyContent:"center"}}>
+      <h1>🦉 HooHacks Owl Mail ✉️</h1>
+      <h2>To Name/Organization:</h2>
+      <h2>To Email Address:</h2>
+      <h2>Class Name:</h2>
+      <h2>Your Name:</h2>
+
+    </div>
+  );
+}
+
+export default App;
